f7b048197d84240b83a5ecd216e8980c7c460fb1
jacopolegovini <legovini96@gmail.com>
2024-07-30 15:16:27 +0200

    exercise completed

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ console.log('JS OK')
 
 /* 
 Collegarsi al DOM e recuperare gli elementi
+Dichiarare le variabili
 Raccogliere i dati
 Concatenazione stringhe
 Produzione output
@@ -9,3 +10,12 @@ Produzione output
 
 const passwordGenerator = document.getElementById('password-generator');
 console.log(passwordGenerator)
+
+const randomNumber = 21;
+
+const firstname = prompt('Qual è il tuo nome?', 'jacopo');
+const lastname = prompt('Qual è il tuo cognome?', 'legovini');
+const favoriteColor = prompt('Qual è il tuo colore preferito?', 'blue');
+
+passwordGenerator.innerText = firstname + lastname + favoriteColor + randomNumber;
+
