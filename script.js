3a306f2e3b307217995a9ce17fec5e3cf75dc1ef
jacopolegovini <legovini96@gmail.com>
2024-07-30 15:09:43 +0200

    added comments

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('JS OK')
+
+/* 
+Collegarsi al DOM e recuperare gli elementi
+Raccogliere i dati
+Concatenazione stringhe
+Produzione output
+*/
+
+const passwordGenerator = document.getElementById('password-generator');
+console.log(passwordGenerator)
